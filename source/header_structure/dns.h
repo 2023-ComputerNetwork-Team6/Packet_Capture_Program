5eae6054e019c094a79f82ce60e20103e22aadb5
ksh-g001 <kshyunn12@gmail.com>
2023-11-22 18:21:49 +0900

    File : make protocol header file

---
+++ source/header_structure/dns.h
@@ -0,0 +1,17 @@
+struct dnsHeader{
+    unsigned short id;
+    unsigned char rd : 1;
+    unsigned char tc : 1;
+    unsigned char aa : 1;
+    unsigned char opcode : 4;
+    unsigned char qr : 1;
+    unsigned char rcode : 4;
+    unsigned char cd : 1;
+    unsigned char ad : 1;
+    unsigned char z : 1;
+    unsigned char ra : 1;
+    unsigned short qdcount;
+    unsigned short ancount;
+    unsigned short nscount;
+    unsigned short arcount;
+};
---
+++ source/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+void menuManager(){
+    뉴
+}
+
+void menuPrint(){
+    printf("=========== 메")
+}
