e75e2aa9b511656574623c88f2968c1f531b5959
eJwtzD1vwyAUheH/wuwBxx8xbOnQoR+qKnXqgq7hOiYGQy6maRTlv9eNvB0d6XlvDLTGlL7ChDOTjO9rMEJUZdnu+L4zDUI1DJUWvBEdL/umGdqu7jgrWKTwYw3SRm9bSS1b6jgG5ceX8XyK41v2rciv7y0c5s98Pnz43Yl/19WzfaI19SBquUZcXY9A+P8mHR4HGG9nGXPvrFYTXgtCMDLQcR0xFDkhSfRgXXEJNA0uXFaMv9ESJgULk3N27n7/A+NnT5U= <kshyunn12@gmail.com>
2023-11-22 18:57:55 +0900

    Feat : add UI

--- source/main.c
+++ source/main.c
@@ -1,14 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+
+#define TCP 't'
+#define UDP 'u'
+#define ICMP 'i'
+#define DNS 'd'
+#define HTTP 'h'
+#define SSH 's'
+#define NONE 'n'
+
+void menuPrint();
+void menuManager();
+void captureManager(char nt, char at);
 
 int main() {
-    printf("Hello, World!\n");
+    menuManager();
     return 0;
 }
 
 void menuManager(){
-    뉴
+    int menu = -1;
+    while(1){
+        menuPrint();
+        scanf("%d", &menu);
+        switch (menu) {
+            case 1:
+                captureManager(ICMP, NONE);
+                break;
+            case 2:
+                captureManager(UDP, DNS);
+                break;
+            case 3:
+                captureManager(TCP, HTTP);
+                break;
+            case 4:
+                captureManager(TCP, SSH);
+                break;
+            case 5:
+
+                break;
+            case 0:
+                exit(0);
+            default:
+                printf("존재하지 않는 기능입니다.\n");
+        }
+    }
 }
 
 void menuPrint(){
-    printf("=========== 메")
+    printf("=========== 메뉴 ===========\n");
+    printf("1. ICMP 패킷 분석\n");
+    printf("2. DNS 패킷 분석\n");
+    printf("3. HTTP 패킷 분석\n");
+    printf("4. SSH 패킷 분석\n");
+    printf("5. 화면 비우기\n");
+    printf("0. 종료\n");
+    printf("==========================\n");
+    printf("메뉴 선택 : ");
+}
+
+void captureManager(char nt, char at){
+    if(nt == ICMP){
+
+    }else if(nt == UDP){
+
+    }else if(nt == TCP){
+        if(at == HTTP){
+
+        }else if(at == SSH){
+
+        }
+    }
 }
