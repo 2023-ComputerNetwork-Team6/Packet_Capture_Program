1e5f37459f88df6ebba8807633d6f757dfc151ff
ksh-g001 <kshyunn12@gmail.com>
2023-11-30 05:16:29 +0900

    Feat : Add function to convert hex to ASCII code
    시간 관계상 아직 ssh는 제대로 테스트하지 못했습니다.
    그러나, DNS, HTTP는 무사히 나오는 걸 확인했습니다.
    UI나, 간편 출력 형태를 만들실 분들께서는 자유롭게 참여하셔도 상관없습니다. UI에 색을 추가했습니다.

--- source/main.c
+++ source/main.c
@@ -8,6 +8,7 @@
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 #include "log/logQueue.h"
 
 #define IP 8
@@ -21,6 +22,15 @@
 #define MAX_PACKET_SIZE 65536
 #define MAX_FILE_NAME 30
 #define INIT_QUEUE_SIZE 0
+#define ASCCI_START_NUM 32
+#define ASCCI_END_NUM 128
+#define HEX 16
+
+enum {
+    red = 1,
+    yellow = 3,
+    blue = 4
+};
 
 void menuManager();
 void menuPrint();
@@ -33,6 +43,8 @@ void ipCapture(struct LogQueue* q, struct iphdr* iph);
 void tcpCapture(struct LogQueue* q, struct tcphdr* th);
 void udpCapture(struct LogQueue* q, struct udphdr* uh);
 void icmpCapture(struct LogQueue* q, struct icmphdr* ih);
+void printApplicationLayerProtocol(struct LogQueue* q, int p);
+void hexChangeToAscii(struct LogQueue* q, unsigned char* pl, int len);
 
 void saveCaptureManager();
 void saveCapture(char* fn);
@@ -42,8 +54,9 @@ struct LogQueue lq;
 
 int main() {
     initializeLogQueue(&lq, INIT_QUEUE_SIZE);
-    printf("******* Packet Capture Program *******\n");
-    printf("[자세한 작동 방식은 사용법을 참조하세요.]\n");
+    system("clear");
+    printf("***************  Packet Capture Program  ***************\n");
+    printf("\n\033[0;3%dm - 버전 : 1.0.0\n - 가동 가능 환경 : Linux\n - 권장 운영체제 : Ubuntu \n - 현재 로그 파일 저장 위치 : 프로그램 설치 폴더\n - 자세한 작동 방식은 사용법을 참조하세요.\033[0m\n\n", yellow);
     menuManager();
     return 0;
 }
@@ -54,6 +67,9 @@ void menuManager(){
     int start = 0;
     pthread_t ct;
     while(1){
+        if(lq.front != NULL){
+            printf("\n임시 저장 데이터가 존재합니다!\n");
+        }
         menuPrint();
         scanf("%d", &menu);
         scanf("%*c");           //버퍼 비우기
@@ -62,7 +78,7 @@ void menuManager(){
                 start = 1;
                 recvStatus = 1;
                 if((rs = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1){
-                    printf("[오류] raw socket 생성에 실패했습니다. 프로그램을 종료합니다.");
+                    printf("[ \033[0;3%dm오류\033[0m ] raw socket 생성에 실패했습니다. 프로그램을 종료합니다.", red);
                     exit(1);
                 }
                 ct = pthread_create(&ct, NULL, captureThread, (void *)&rs);
@@ -80,17 +96,23 @@ void menuManager(){
                 system("clear");
                 break;
             case 9:
-                printf("[주의] 해당 기능을 사용할 시 기존에 저장된 분석 결과는 삭제됩니다.\n계속 진행하시겠습니까? [y/n] ");
+                if(recvStatus == 1){
+                    printf("[ \033[0;3%dm!경고!\033[0m ] 패킷 분석 도중에는 해당 기능을 이용하실 수 없습니다.", red);
+                    break;
+                }
+                printf("[ \033[0;3%dm주의\033[0m ] 해당 기능을 사용할 시 기존에 저장된 분석 결과는 삭제됩니다.\n계속 진행하시겠습니까? [y/n] ", yellow);
                 int answer = getchar();
                 if(answer == 'y'){
                     clear(&lq);
-                    printf("* 0 이하의 값을 입력하시면 초기 사이즈(%d줄) 설정으로 돌아갑니다.\n   [현재 사이즈 : %d줄]\n", MAX_QUEUE_SIZE, lq.maxSize);
+                    printf("\033[0;3%dm* 0 이하의 값을 입력하시면 초기 사이즈(%d줄) 설정으로 돌아갑니다.\n* 임시 저장공간이 꽉차도 분석이 중단되지 않습니다!\033[0m\n\t[ \033[0;3%dm현재 사이즈 : %d줄\033[0m ]\n",yellow, MAX_QUEUE_SIZE, blue, lq.maxSize);
                     printf("입력 값 : ");
                     scanf(" %d", &answer);
                     initializeLogQueue(&lq, answer);
                 }
                 break;
             case 0:
+                close(rs);
+
                 exit(0);
             default:
                 printf("존재하지 않는 기능입니다.\n");
@@ -99,13 +121,13 @@ void menuManager(){
 }
 
 void menuPrint(){
-    printf("============ 메뉴 ============\n");
+    printf("========================= 메뉴 =========================\n");
     printf("1. 패킷 분석\n");
-    printf("2. 분석 종료\n");
+    printf("2. 분석 종료 및 결과 저장\n");
     printf("3. 화면 비우기\n");
     printf("9. 최대 임시 저장 줄 설정\n");
-    printf("0. 종료\n");
-    printf("=============================\n");
+    printf("\033[0;3%dm0. 종료\033[0m\n", red);
+    printf("========================================================\n");
     printf("메뉴 선택 : ");
 }
 
@@ -116,7 +138,7 @@ void* captureThread(void* arg){
     while(recvStatus){
         data = recvfrom(rs, buf, MAX_PACKET_SIZE, 0, NULL, NULL);
         if(data == -1){
-            printf("[오류] 데이터를 수신과정에서 오류가 발생했습니다.\n패킷 분석을 강제종료합니다.");
+            printf("[ \033[0;3%dm오류\033[0m ] 데이터를 수신과정에서 오류가 발생했습니다.\n패킷 분석을 강제종료합니다.", yellow);
             pthread_exit(NULL);
         }
         captureManager(&lq, buf);
@@ -131,7 +153,8 @@ void captureManager(struct LogQueue* q, char* buf){
 
     if(ethernetHeader->h_proto == IP){
         struct iphdr* ipHeader = (struct iphdr*)(buf + ETH_HLEN);
-        int overloadLength = ETH_HLEN + (ipHeader->ihl*4);
+        int ipHeaderLength = (ipHeader->ihl*4);
+        int overloadLength = ETH_HLEN + ipHeaderLength;
         int ipTotalLength = ntohs(ipHeader->tot_len);
         ipCapture(q, ipHeader);
 
@@ -144,27 +167,25 @@ void captureManager(struct LogQueue* q, char* buf){
             uint16_t sourcePort = ntohs(tcpHeader->th_sport);
             uint16_t destPort = ntohs(tcpHeader->th_dport);
             int tcpHeaderLength = (tcpHeader->th_off*4);
-            int payloadLength = ipTotalLength - tcpHeaderLength;
-            if(sourcePort == HTTP && payloadLength>0){
-                struct httpPacket* httpPacket = (struct httpPacket*)(buf + overloadLength + tcpHeaderLength);
-
-            }else if(destPort == HTTP && payloadLength>0){
-
-            }else if(sourcePort == SSH){
-
-            }else if(destPort == SSH){
-
+            int payloadLen = ipTotalLength - ipHeaderLength - tcpHeaderLength;
+            if(sourcePort == HTTP || destPort == HTTP){
+                printApplicationLayerProtocol(q, HTTP);
+                hexChangeToAscii(q, buf + overloadLength + tcpHeaderLength, payloadLen);
+            }else if(sourcePort == SSH || destPort == SSH) {
+                printApplicationLayerProtocol(q, SSH);
+                hexChangeToAscii(q, buf+overloadLength+tcpHeaderLength, payloadLen);
             }
-
         }else if(ipHeader->protocol == UDP){
             struct udphdr* udpHeader = (struct udphdr*)(buf + overloadLength);
             udpCapture(&lq, udpHeader); // UDP 헤더 분석 함수 호출
             uint16_t sourcePort = ntohs(udpHeader->uh_sport);
-            uint16_t destPort = ntohs(udpHeader->uh_dport); 
-            if(sourcePort == DNS){
-
-            }else if(destPort == DNS){
-
+            uint16_t destPort = ntohs(udpHeader->uh_dport);
+            int udpPacketLength = ntohs(udpHeader->uh_ulen);
+            int udpHeaderLength = sizeof(struct udphdr);
+            int payloadLength = udpPacketLength - udpHeaderLength;
+            if(sourcePort == DNS || destPort == DNS) {
+                printApplicationLayerProtocol(q, DNS);
+                hexChangeToAscii(q, buf + overloadLength + udpHeaderLength, payloadLength);
             }
         }
     }
@@ -304,18 +325,76 @@ void udpCapture(struct LogQueue* q, struct udphdr* uh) {
     printf("%s", udpBuf);
 }
 
+void printApplicationLayerProtocol(struct LogQueue* q, int p){
+    switch (p) {
+        case DNS :
+            enqueue(q, "[DNS]\n");
+            printf("[DNS]\n");
+            break;
+        case HTTP :
+            enqueue(q, "[HTTP]\n");
+            printf("[HTTP]\n");
+            break;
+        case SSH :
+            enqueue(q, "[SSH]\n");
+            printf("[SSH]\n");
+            break;
+    }
+}
+
+void hexChangeToAscii(struct LogQueue* q, unsigned char* pl, int len){
+    char buf[MAX_DATA_SIZE]={0};
+    char temp[MAX_DATA_SIZE]={0};
+    for (int i = 0; i < len; i++) {
+        strcat(buf, "\t\t");
+        if (i != 0 && i % HEX == 0) {
+            for (int j = i - HEX; j < i; j++) {
+                if (pl[j] >= ASCCI_START_NUM && pl[j] < ASCCI_END_NUM){
+                    sprintf(temp, "%c", pl[j]);
+                    strncat(buf, temp, 1);
+                }else
+                    strcat(buf, ".");
+            }
+            strcat(buf, "\n");
+            enqueue(q, buf);
+            printf("%s", buf);
+            memset(buf, '\0', sizeof(buf));
+        }
+        if (i % 16 == 0)
+            strcat(buf, "\t");
+        sprintf(temp, "%02X", (unsigned int)pl[i]);
+        if (i == len - 1){
+            for (int j = 0; j < ((HEX - 1) - (i % HEX)); j++)
+                strcat(buf, "   ");
+            strcat(buf, "\t\t");
+            for (int j = (i - (i % HEX)); j <= i; j++){
+                if (pl[j] >= ASCCI_START_NUM && pl[j] < ASCCI_END_NUM){
+                    sprintf(temp, "%c", pl[j]);
+                    strncat(buf, temp, 1);
+                }
+                else
+                    strcat(buf, ".");
+            }
+            strcat(buf, "\n");
+            enqueue(q, buf);
+            printf("%s", buf);
+            memset(buf, '\0', sizeof(buf));
+        }
+    }
+}
+
 void saveCaptureManager(){
     printf("분석 내용을 저장하시겠습니까? [y/n] ");
     int answer;
     while(1){
         answer = getchar();
-        if(answer == 'y'){
+        if(answer == 'y' | answer == 'Y'){
             char name[MAX_FILE_NAME];
             printf("파일 이름을 입력해주세요 : ");
             scanf(" %s", name);
             saveCapture(name);
             break;
-        }else if(answer == 'n'){
+        }else if(answer == 'n' | answer == 'Y'){
             break;
         }else{
             printf("다시 입력해주세요.\n");
@@ -330,12 +409,12 @@ void saveCapture(char* fn){
     strcat(fileName, txt);
     FILE* captureLog = fopen(fileName, "w");
     if(captureLog == NULL){
-        printf("[오류] 파일을 저장할 수 없습니다.\n프로그램을 강제종료합니다.\n");
+        printf("[ \033[0;3%dm오류\033[0m ] 파일을 저장할 수 없습니다.\n프로그램을 강제종료합니다.\n", yellow);
         return;
     }
     unsigned int size = lq.size;
 
-    printf("%s 파일을 저장하는 중입니다.\n\n[전체 %d 줄]\n", fileName, size);
+    printf("%s 파일을 저장하는 중입니다.\n\n[ \033[0;3%dm전체 %d 줄\033[0m ]\n", fileName, blue, size);
     while(lq.front != NULL){
         char* data = strdup(dequeue(&lq));
         fprintf(captureLog, "%s\n", data);
